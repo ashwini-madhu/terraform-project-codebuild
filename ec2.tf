6a7b9c681a594fd5786892d8b161c62778a18586
ashwini-madhu <ashwini.achu90@gmail.com>
2025-02-17 11:07:42 +0530

    created new files

--- ec2.tf
+++ ec2.tf
@@ -87,7 +87,7 @@ resource "aws_instance" "ec2_instance" {
   subnet_id              = aws_default_subnet.default_az1.id
   vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
   key_name               = "new-key-pair"
-  user_data              = file("install.sh")
+  user_data              = file("install-techmax.sh")
 
   tags = {
     Name = "techmax server"
